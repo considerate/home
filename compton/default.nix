904066230bde33f4be147c672fe955114735cca2
Viktor Kronvall <viktor.kronvall@gmail.com>
2021-06-15 21:31:06 +0900

    Disable compton

--- compton/default.nix
+++ compton/default.nix
@@ -1,7 +1,16 @@
 { pkgs, lib, config, ... }:
 {
   services.picom = {
-    enable = true;
+    enable = false;
+    package = pkgs.picom.overrideAttrs (_: {
+      src = pkgs.fetchFromGitHub {
+        repo = "picom";
+        owner = "yshui";
+        rev = "d974367a0446f4f1939daaada7cb6bca84c893ef";
+        sha256 = "0mjm544vck493sdcvr90d9ycg5sxin28a13w61q1kqkycilv87lv";
+      };
+    });
+    experimentalBackends = true;
     vSync = true;
     shadow = true;
     fade = true;
