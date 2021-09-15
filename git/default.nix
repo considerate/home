f630a327afc23c15eb9ff289c554ada143287876
Viktor Kronvall <viktor.kronvall@gmail.com>
2021-09-16 05:54:51 +0900

    Write image-diff as a bash script
    I couldn't figure out how to do a temporary file inline in the git cmd
    definition.

--- git/default.nix
+++ git/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 let
 in
 {
@@ -6,19 +6,31 @@ in
     pkgs.gitg
   ];
   programs = {
-    git = {
-      enable = true;
-      userName = "Viktor Kronvall";
-      userEmail = "viktor.kronvall@gmail.com";
-      ignores = [ "tags" "*.bak" "worktree" ".envrc" ];
-      extraConfig = {
-        commit.verbose = true;
-        push.default = "current";
-        merge.tool = "fugitive";
-        alias.diff-img = "difftool -t image-diff";
-        difftool.image-diff.cmd = ''exec 10>out ; ${pkgs.imagemagick}/bin/compare $REMOTE $LOCAL png:- | ${pkgs.imagemagick}/bin/montage -geometry 400x -font Liberation-Sans -label 'reference' $LOCAL -label 'diff' - -label 'current--%f' $REMOTE /dev/fd/10; ${pkgs.sxiv}/bin/sxiv /dev/fd/10'';
-        mergetool.fugitive.cmd = ''nvim -f -c "Gvdiffsplit!" "$MERGED"'';
+    git =
+      let
+        image-diff = pkgs.writeScript "image-diff.sh" ''
+          tmpfile=$(mktemp -t image-diff.XXXXXXX);
+          function cleanup() {
+             rm -f "$tmpfile"
+          };
+          trap cleanup EXIT;
+          ${pkgs.imagemagick}/bin/compare $1 $2 png:- | ${pkgs.imagemagick}/bin/montage -geometry 400x -font Liberation-Sans -label 'reference' $2 -label 'diff' - -label 'current--%f' $1 "$tmpfile";
+          ${pkgs.sxiv}/bin/sxiv "$tmpfile"
+        '';
+      in
+      {
+        enable = true;
+        userName = "Viktor Kronvall";
+        userEmail = "viktor.kronvall@gmail.com";
+        ignores = [ "tags" "*.bak" "worktree" ".envrc" ];
+        extraConfig = {
+          commit.verbose = true;
+          push.default = "current";
+          merge.tool = "fugitive";
+          alias.diff-img = "difftool -t image-diff";
+          difftool.image-diff.cmd = "${image-diff} $REMOTE $LOCAL";
+          mergetool.fugitive.cmd = ''nvim -f -c "Gvdiffsplit!" "$MERGED"'';
+        };
       };
-    };
   };
 }
