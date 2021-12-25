ccd4eb663512affade98bc982d33333b29d1f78f
Viktor Kronvall <viktor.kronvall@gmail.com>
2021-12-26 05:35:10 +0900

    Remove deprecated rofi attributes

--- rofi/default.nix
+++ rofi/default.nix
@@ -5,28 +5,28 @@
         plugins = [ pkgs.rofi-calc ];
       };
       enable = config.considerate.desktop;
-      padding = 24;
-      borderWidth = 2;
-      separator = "dash";
       font = "Fira Code Retina 24";
-      colors = {
-        window = {
-          background = "argb:ee343d46";
-          border = "argb:ff2b303b";
-          separator = "argb:ff2b303b";
-        };
-        rows = {
-          normal = {
-            background = "argb:002b303b";
-            foreground = "#dfe1e8";
-            backgroundAlt = "argb:002b303b";
-            highlight = {
-              background = "argb:002b303b";
-              foreground = "#bf616a";
-            };
-          };
-        };
-      };
+      # padding = 24;
+      # borderWidth = 2;
+      # separator = "dash";
+      # colors = {
+      #   window = {
+      #     background = "argb:ee343d46";
+      #     border = "argb:ff2b303b";
+      #     separator = "argb:ff2b303b";
+      #   };
+      #   rows = {
+      #     normal = {
+      #       background = "argb:002b303b";
+      #       foreground = "#dfe1e8";
+      #       backgroundAlt = "argb:002b303b";
+      #       highlight = {
+      #         background = "argb:002b303b";
+      #         foreground = "#bf616a";
+      #       };
+      #     };
+      #   };
+      # };
       extraConfig = {
         display-combi = "Go";
         modi = "combi,calc";
