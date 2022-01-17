ebbd992e81224a00da352ccb4e8c102c9a3f6054
Viktor Kronvall <viktor.kronvall@gmail.com>
2022-01-18 06:00:01 +0900

    xmonad: wrap with PATH and log files

--- xmonad/default.nix
+++ xmonad/default.nix
@@ -1,35 +1,63 @@
 { config, lib, pkgs, ... }:
-lib.mkIf config.considerate.desktop {
-  gtk.enable = true;
-  home.packages = [ pkgs.xmobar ];
-  xsession = {
-    enable = true;
-    windowManager.xmonad = {
-      enable = true;
-      config = ./xmonad.hs;
-      enableContribAndExtras = true;
-      extraPackages = hpkgs: with hpkgs; [
-        (xmobar.overrideAttrs
-          (old: {
-            configureFlags = (old.configureFlags or [ ]) ++ [ "-fwith_xpm" ];
-          }))
-      ];
-    };
-    pointerCursor = {
-      package = pkgs.capitaine-cursors;
-      name = "capitaine-cursors";
-      defaultCursor = "left_ptr";
-      size = 48;
+let wrapXmonad = old: pkgs.runCommand "xmonad-wrapper"
+  {
+    nativeBuildInputs = [
+      pkgs.makeWrapper
+      pkgs.gnused
+    ];
+  } ''
+  makeWrapper ${old} $out --prefix PATH : ${lib.makeBinPath [
+    pkgs.brightnessctl
+    pkgs.coreutils
+    pkgs.playerctl
+    pkgs.kbdlight
+    pkgs.alsaUtils
+  ]}
+  sed -i '$s/$/ >\$HOME\/.xmonad.log 2>\$HOME\/.xmonad-errors.log/' $out
+'';
+in
+{
+  options = {
+    # wrap the xmonad binary with some extra PATH
+    xsession.windowManager.command = lib.mkOption {
+      apply = wrapXmonad;
     };
   };
-  home.file = {
-    xmobar-icons = {
-      source = ./xmobar-icons;
-      target = ".xmonad/icons";
+  config = lib.mkIf config.considerate.desktop {
+    gtk.enable = true;
+    home.packages = [ pkgs.xmobar ];
+    xsession = {
+      enable = true;
+      windowManager.xmonad = {
+        enable = true;
+        config = ./xmonad.hs;
+        enableContribAndExtras = true;
+        extraPackages = hpkgs: with hpkgs; [
+          (xmobar.overrideAttrs
+            (old: {
+              configureFlags = (old.configureFlags or [ ]) ++ [ "-fwith_xpm" ];
+            }))
+        ];
+      };
+      pointerCursor = {
+        package = pkgs.capitaine-cursors;
+        name = "capitaine-cursors";
+        defaultCursor = "left_ptr";
+        size = 48;
+      };
     };
-    xmobarrc = {
-      source = ./xmobarrc;
-      target = ".xmobarrc";
+    home.file = {
+      ".xmonad/xmonad-${pkgs.stdenv.hostPlatform.system}" = {
+        source = lib.mkForce config.xsession.windowManager.command;
+      };
+      xmobar-icons = {
+        source = ./xmobar-icons;
+        target = ".xmonad/icons";
+      };
+      xmobarrc = {
+        source = ./xmobarrc;
+        target = ".xmobarrc";
+      };
     };
   };
 }
