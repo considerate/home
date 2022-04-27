3bd6872a26f237c2ddbd9092ae4f2884a31920cd
Viktor Kronvall <viktor.kronvall@gmail.com>
2022-04-27 18:03:39 +0900

    deploy: Bash CLI argparse

--- deploy-cross-compass/deploy.nix
+++ deploy-cross-compass/deploy.nix
@@ -6,21 +6,50 @@ writeShellScriptBin "deploy-cross-compass" ''
   trap finish EXIT
   eval $(ssh-agent)
   key="${if keyPath != null then toString keyPath else "$DEPLOY_SSH_KEY"}"
+  declare -a machines=()
+  while [ -n "$1" ]; do
+    case "$1" in
+      --command)
+        shift
+        command="$1"
+        shift
+      ;;
+      --key)
+        shift
+        key="$1"
+        shift
+      ;;
+      --branch)
+        shift
+        branch="$1"
+        shift
+      ;;
+      --user)
+        shift
+        user="$1@"
+        shift
+      ;;
+      *)
+        machines+=("$1")
+        shift
+      ;;
+    esac
+  done
   if [ -f "$key" ]; then
     ssh-add "$key"
   fi
-  user=""
-  if [ ! -z $DEPLOY_USER ]; then
-    user="''${DEPLOY_USER}@"
+  if [ -z "$user" ]; then
+    if [ -n "$DEPLOY_USER" ]; then
+      echo "setting to deploy user"
+      user="''${DEPLOY_USER}@"
+    fi
   fi
   echo "Deploying as user: $user"
-  command=''${1:-switch}
-  branch=''${2:-master}
+  command=''${command:-switch}
+  branch=''${branch:-master}
   shift 2
-  machines=(${lib.concatStringsSep " " machines})
-  if [ "$#" -gt 0 ]; then
-    machines=()
-    machines=$@
+  if [ "''${#machines[@]}" -eq 0 ]; then
+    machines=(${lib.concatStringsSep " " machines})
   fi
   echo "deploying branch $branch with nixos-rebuild $command on ''${machines[@]}"
   pids=()
