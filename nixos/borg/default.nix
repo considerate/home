1b7e7d739693f4b0a3d2324f1a284f003898ec9a
Viktor Kronvall <viktor.kronvall@gmail.com>
2022-06-20 03:16:00 +0900

    Exclude paths when backing up

--- nixos/borg/default.nix
+++ nixos/borg/default.nix
@@ -1,6 +1,23 @@
+let common-excludes = [
+  # Largest cache dirs
+  ".cache"
+  "*/cache2" # firefox
+  "*/Cache"
+  ".config/Slack/logs"
+  ".config/Code/CachedData"
+  ".container-diff"
+  ".npm/_cacache"
+  "*/node_modules"
+  "*/bower_components"
+  "*/_build"
+  "*/venv"
+  "*/.venv"
+];
+in
 {
   services.borgbackup.jobs.home-considerate = {
     paths = "/home/considerate";
+    exclude = common-excludes ++ [ "Downloads" ];
     encryption.mode = "none";
     environment.BORG_RSH = "ssh -i /home/considerate/.ssh/nas";
     repo = "ssh://considerate@192.168.11.5/mnt/storage/backup/considerate";
