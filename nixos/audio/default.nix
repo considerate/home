850da4b094c2c60f9962756c6b2c6113932046fd
Viktor Kronvall <viktor.kronvall@gmail.com>
2022-06-25 18:13:12 +0900

    add noise cancelling sink to pulseaudio

--- nixos/audio/default.nix
+++ nixos/audio/default.nix
@@ -4,5 +4,10 @@
   hardware.pulseaudio = {
     enable = true;
     # package = pkgs.pulseaudio-hsphfpd;
+    extraConfig = ''
+      load-module module-echo-cancel source_name=noechosource sink_name=noechosink
+      set-default-source noechosource
+      set-default-sink noechosink
+    '';
   };
 }
