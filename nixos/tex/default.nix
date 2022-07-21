563a78a202c1341834507972d5a55aa9e7378f0f
Viktor Kronvall <viktor.kronvall@gmail.com>
2022-07-22 03:02:25 +0900

    enable some more latex packages

--- nixos/tex/default.nix
+++ nixos/tex/default.nix
@@ -3,8 +3,9 @@ let
   considerate-texlive = with pkgs;
     texlive.combine {
       inherit (texlive)
-        beamer beamertheme-metropolis biblatex blindtext csquotes emptypage
-        logreq media9 ocgx2 pgfopts scheme-medium titlesec wrapfig;
+        todonotes cleveref beamer beamertheme-metropolis biblatex blindtext
+        csquotes emptypage logreq media9 ocgx2 pgfopts scheme-medium titlesec
+        wrapfig;
     };
 in
 {
