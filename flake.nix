22c701480c076ce65a5aaa9fda9f9c527806ef2f
Viktor Kronvall <viktor.kronvall@gmail.com>
2022-10-15 00:32:26 +0900

    format flake

--- flake.nix
+++ flake.nix
@@ -8,44 +8,43 @@
     agenix.url = "github:ryantm/agenix";
     agenix.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = inputs:
-    {
-      nixosModules = import ./nixos inputs;
-      homeModules = import ./home.nix inputs;
-      homeConfigurations = {
-        considerate = inputs.home-manager.lib.homeManagerConfiguration {
-          system = "x86_64-linux";
-          homeDirectory = "/home/considerate";
-          username = "considerate";
-          stateVersion = "22.05";
-          configuration = inputs.self.homeModules.considerate;
-        };
+  outputs = inputs: {
+    nixosModules = import ./nixos inputs;
+    homeModules = import ./home.nix inputs;
+    homeConfigurations = {
+      considerate = inputs.home-manager.lib.homeManagerConfiguration {
+        system = "x86_64-linux";
+        homeDirectory = "/home/considerate";
+        username = "considerate";
+        stateVersion = "22.05";
+        configuration = inputs.self.homeModules.considerate;
       };
-      nixosConfigurations = {
-        considerate-nixos = import machines/considerate-nixos.nix inputs;
-      };
-      apps =
-        let
-          pkgs = import inputs.nixpkgs {
-            system = "x86_64-linux";
-          };
-          deploy-cross-compass = pkgs.callPackage ./deploy-cross-compass/deploy.nix {
-            machines = [
-              "jarjar.xc"
-              "chewbacca.xc"
-              "anakin.xc"
-              "pichanaki.xc"
-              "bobafett.xc"
-            ];
-          };
-        in
-        {
-          x86_64-linux = {
-            deploy-cross-compass = {
-              type = "app";
-              program = "${deploy-cross-compass}/bin/deploy-cross-compass";
-            };
-          };
-        };
     };
+    nixosConfigurations = {
+      considerate-nixos = import machines/considerate-nixos.nix inputs;
+    };
+    apps =
+      let
+        pkgs = import inputs.nixpkgs {
+          system = "x86_64-linux";
+        };
+        deploy-cross-compass = pkgs.callPackage ./deploy-cross-compass/deploy.nix {
+          machines = [
+            "jarjar.xc"
+            "chewbacca.xc"
+            "anakin.xc"
+            "pichanaki.xc"
+            "bobafett.xc"
+          ];
+        };
+      in
+      {
+        x86_64-linux = {
+          deploy-cross-compass = {
+            type = "app";
+            program = "${deploy-cross-compass}/bin/deploy-cross-compass";
+          };
+        };
+      };
+  };
 }
