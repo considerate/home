7110f7722c21bb54e9ee7eab39ebd70037dd0b58
Viktor Kronvall <viktor.kronvall@gmail.com>
2023-02-16 02:47:29 +0900

    remove purifyeps from texlive output

--- nixos/tex/default.nix
+++ nixos/tex/default.nix
@@ -1,12 +1,16 @@
 { pkgs, ... }:
 let
   considerate-texlive = with pkgs;
-    texlive.combine {
+    (texlive.combine {
       inherit (texlive)
         todonotes cleveref beamer beamertheme-metropolis biblatex blindtext
         csquotes emptypage logreq media9 ocgx2 pgfopts scheme-medium titlesec
         wrapfig;
-    };
+    }).overrideAttrs (old: {
+      postBuild = (old.postBuild or "") + ''
+        rm $out/bin/purifyeps
+      '';
+    });
 in
 {
   environment.systemPackages = [
